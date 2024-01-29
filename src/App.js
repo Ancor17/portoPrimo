36d4e2ad6d355125d028ea4c51962fec8b09c64f
Aurora\Игорь <igor-strong2011@mail.ru>
2024-01-29 17:46:10 +0300

    1-st stap in solo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-const App = () => {
+function App() {
+
+    const [likes, setLikes] = useState(0)
+    const [value, setValue] = useState('NIGGERS')
+
+    function increment(){
+       setLikes(likes + 1)
+    }
+    function decrement(){
+        setLikes(likes - 1)
+    }
+
     return (
-        <div>
-          Base 2
+
+        <div className="App">
+            <h1>{likes}</h1>
+            <h1>{value}</h1>
+            <input type='text'
+                   value={value}
+                   onChange={event => setValue(event.target.value)}
+            />
+            <button onClick={increment}> Increment </button>
+            <button onClick={decrement}> Decrement </button>
         </div>
+
     );
 };
 
